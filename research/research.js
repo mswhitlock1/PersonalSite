d95290fe5612568842434c0885ece6816e7b7877
Matt Whitlock <matthew.whitlock@colorado.edu>
2017-12-18 10:24:19 -0700

    Deploy 0.4

--- research/research.js
+++ research/research.js
@@ -53,7 +53,7 @@ angular.module('myApp.research', ['ngRoute'])
             description: "I led a team working on a project that doubled as an exercise in User-Centered Design.  Our group explored the potential for use of augmented reality to blend the virtual and physical shopping experiences.  We went through the stages of interviews and prototyping to iteratively work toward a final prototype, built in virtual reality."
         },
         {
-            img: "img/ZayoDashboard.png",
+            img: "img/research/ZayoDashboard.png",
             img_link: "",
             link_name: "",
             description: "As a part of a class project for Information Visaulization, we partnered with Zayo to visualize hypothetical mapdata which could inform them on where they may expand their customer base.  Our group presented to Zayo and won second place for our visualization tools and findings."
